164f4301ecb1665c9fdcfae9c179126911821b8e
Tasdiq-Dewan <tasdiqd@gmail.com>
2022-08-18 10:17:02 +0100

    removed subdoc ids

--- routes/mongoAPI.js
+++ routes/mongoAPI.js
@@ -21,7 +21,7 @@ const actorSchema = new Schema({
         type: Number,
         required: true
     }
-});
+}, {_id: false});
 
 const reviewSchema = new Schema({
     rating: {
@@ -34,7 +34,7 @@ const reviewSchema = new Schema({
         type: String,
         required: true
     }
-});
+}, {_id: false});
 
 const movieSchema = new Schema({
     title: {
@@ -69,7 +69,13 @@ router.post("/create", (req, res) => {
 });
 
 router.get("/getAll", (req, res) => {
-    movieModel.find({}).select("-_id -__v")
+    movieModel.find({})
+    .then((movies) => res.send(JSON.stringify(movies)))
+    .catch(err => { console.log(err)});
+})
+
+router.get("/getAllClean", (req, res) => {
+    movieModel.find({}).select("-_id -_v")
     .then((movies) => res.send(JSON.stringify(movies)))
     .catch(err => { console.log(err)});
 })
@@ -80,8 +86,26 @@ router.get("/getByName/:name", (req, res) => {
     .catch(err => { console.log(err)});
 })
 
+router.get("getById/:id", (req, res) => {
+    movieModel.findOne({_id: req.params.id})
+    .then((movie) => res.send(JSON.stringify(movies)))
+    .catch(err => { console.log(err)});
+});
+
+router.delete("deleteById/:id", (req, res) => {
+    movieModel.deleteOne({_id: req.params.id})
+    .then((ok) => res.send(JSON.stringify(ok)))
+    .catch(err => {console.log(err)});
+});
+
 router.delete("/deleteByTitle/:name", (req, res) => {
     movieModel.deleteOne({title: req.params.name})
+    .then((ok) => res.send(JSON.stringify(ok)))
+    .catch(err => {console.log(err)});
+})
+
+router.delete("/deleteAll", (req, res) => {
+    movieModel.collection.drop()
     .then((ok) => res.send(JSON.stringify(ok)))
     .catch(err => {console.log(err)});
 })
@@ -93,7 +117,13 @@ router.put("/updateTitle/:name", (req, res) => {
 });
 
 router.put("/updateDescription/:title", (req, res) => {
-    movieModel.updateOne({title: req.params.tite}, {description: req.body.description})
+    movieModel.updateOne({title: req.params.title}, {description: req.body.description})
+    .then((updated) => res.send(JSON.stringify(updated)))
+    .catch(err => {console.log(err)});
+});
+
+router.put("/addReview/:title", (req, res) => {
+    movieModel.updateOne({title: req.params.title}, {"$push": {reviews: req.body}})
     .then((updated) => res.send(JSON.stringify(updated)))
     .catch(err => {console.log(err)});
 });
